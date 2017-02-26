b8c863994906257248c03d23ee89183c4c896573
HTS <HTS@ASUS>
2017-02-26 19:23:10 +0800

    第一次提交

---
+++ src/servlet/AjaxLogin.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxLogin
+ */
+@WebServlet("/AjaxLogin")
+public class AjaxLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String username= request.getParameter("username");
+		//username= java.net.URLDecoder.decode(username, "UTF-8");		  
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();	
+		if(username.equals("abc")){
+			out.print("’À∫≈∫œ∑®");
+		}else{
+			out.print("¥ÌŒÛ");
+		}
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
